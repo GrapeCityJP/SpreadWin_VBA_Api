8e47323b38cffa17aa113b77a1ea97d0265ff901
GrapeCity JP Developer Tools Marketing <48461005+GrapeCityJPDevTools@users.noreply.github.com>
2021-10-22 15:24:28 +0900

    new create

---
+++ SpreadWin_VBAApi/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FpSpread1 = New FarPoint.Win.Spread.FpSpread(FarPoint.Win.Spread.LegacyBehaviors.None, CType(resources.GetObject("resource1"), System.IO.MemoryStream))
+        Me.FpSpread1_Sheet1 = Me.FpSpread1.GetSheet(0)
+        CType(Me.FpSpread1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(89, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(417, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "リセット"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(278, 12)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "VBA互換API"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(215, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "従来のAPI"
+        '
+        'FpSpread1
+        '
+        Me.FpSpread1.AccessibleDescription = "FpSpread1, Sheet1, Row 0, Column 0"
+        Me.FpSpread1.Font = New System.Drawing.Font("ＭＳ Ｐゴシック", 11.0!)
+        Me.FpSpread1.Location = New System.Drawing.Point(12, 38)
+        Me.FpSpread1.Name = "FpSpread1"
+        Me.FpSpread1.Size = New System.Drawing.Size(480, 211)
+        Me.FpSpread1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(504, 261)
+        Me.Controls.Add(Me.FpSpread1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.FpSpread1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents FpSpread1 As FarPoint.Win.Spread.FpSpread
+    Friend WithEvents FpSpread1_Sheet1 As FarPoint.Win.Spread.SheetView
+End Class
---
+++ SpreadWin_VBAApi/Form1.vb
@@ -0,0 +1,200 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' バージョンの表示
+        Me.Text = $"SPREAD ver.{FpSpread1.ProductVersion}"
+
+        ' コンボボックスの設定
+        ComboBox1.Items.AddRange({"SetValueA", "SetStyleA", "CopyCellA", "ClearCellA", "SelectCellA", "CopySheetA"})
+        ComboBox2.Items.AddRange({"SetValueB", "SetStyleB", "CopyCellB", "ClearCellB", "SelectCellB", "CopySheetB"})
+
+        ' SPREADの初期設定
+        FpSpread1.TabStripRatio = 0.7
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' シートの初期化
+        FpSpread1.Sheets.Clear()
+        Dim sheet = New FarPoint.Win.Spread.SheetView()
+        sheet.Columns.Default.Width = 72
+        FpSpread1.Sheets.Add(sheet)
+
+        ' コンボボックスの初期化
+        ComboBox1.SelectedIndex = -1
+        ComboBox2.SelectedIndex = -1
+    End Sub
+
+    ' 従来の方法による操作
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Select Case ComboBox1.SelectedIndex
+            Case 0
+                Call SetValueA()
+            Case 1
+                Call SetStyleA()
+            Case 2
+                Call CopyCellA()
+            Case 3
+                Call ClearCellA()
+            Case 4
+                Call SelectCellA()
+            Case 5
+                Call CopySheetA()
+        End Select
+    End Sub
+
+    ' VBA互換APIを使う方法による操作
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        Select Case ComboBox2.SelectedIndex
+            Case 0
+                Call SetValueB()
+            Case 1
+                Call SetStyleB()
+            Case 2
+                Call CopyCellB()
+            Case 3
+                Call ClearCellB()
+            Case 4
+                Call SelectCellB()
+            Case 5
+                Call CopySheetB()
+        End Select
+    End Sub
+
+    ' 値の設定：VBA互換APIを使う方法
+    Private Sub SetValueA()
+        With FpSpread1.Sheets(0).AsWorksheet()
+            ' 文字列でセル範囲を指定する方法
+            .Range("A1:B2").Value = "ABC"
+
+            ' インデックス番号でセル範囲を指定する方法
+            .Range({New GrapeCity.Spreadsheet.Reference(2, 2, 3, 3)}).Value = "XYZ"
+        End With
+    End Sub
+
+    ' 値の設定：従来の方法
+    Private Sub SetValueB()
+        With FpSpread1.Sheets(0)
+            ' 文字列でセル範囲を指定する方法
+            .Cells("A1:B2").Value = "ABC"
+
+            ' インデックス番号でセル範囲を指定する方法
+            .Cells(2, 2, 3, 3).Value = "XYZ"
+        End With
+    End Sub
+
+    ' スタイルの設定：VBA互換APIを使う方法
+    Private Sub SetStyleA()
+        With FpSpread1.Sheets(0).AsWorksheet()
+            ' 文字列でセル範囲を指定する方法
+            .Range("A1:B2").Interior.Color = GrapeCity.Spreadsheet.Color.FromArgb(Color.Lavender.ToArgb())
+            .Range("A1:B2").Font.Color = GrapeCity.Spreadsheet.Color.FromArgb(Color.Blue.ToArgb())
+
+            ' インデックス番号でセル範囲を指定する方法
+            .Range({New GrapeCity.Spreadsheet.Reference(2, 2, 3, 3)}).Interior.ColorIndex = (3 - 1)
+            .Range({New GrapeCity.Spreadsheet.Reference(2, 2, 3, 3)}).Font.Color = GrapeCity.Spreadsheet.Color.FromIndexedColor(2 - 1)
+        End With
+    End Sub
+
+    ' スタイルの設定：従来の方法
+    Private Sub SetStyleB()
+        With FpSpread1.Sheets(0)
+            ' 文字列でセル範囲を指定する方法
+            .Cells("A1:B2").BackColor = Color.Lavender
+            .Cells("A1:B2").ForeColor = Color.Blue
+
+            ' インデックス番号でセル範囲を指定する方法
+            .Cells(2, 2, 3, 3).BackColor = Color.Red
+            .Cells(2, 2, 3, 3).ForeColor = Color.White
+        End With
+    End Sub
+
+    ' セル範囲のコピー：VBA互換APIを使う方法
+    Private Sub CopyCellA()
+        With FpSpread1.Sheets(0).AsWorksheet()
+            ' 文字列でセル範囲を指定する方法
+            .Range("A1:B2").Copy(destination:= .Range("A3"))
+
+            ' インデックス番号でセル範囲を指定する方法
+            .Range({New GrapeCity.Spreadsheet.Reference(2, 2, 3, 3)}).Copy(destination:= .Range({New GrapeCity.Spreadsheet.Reference(4, 2, 4, 2)}))
+        End With
+    End Sub
+
+    ' セル範囲のコピー：従来の方法
+    Private Sub CopyCellB()
+        With FpSpread1.Sheets(0)
+            ' 文字列でセル範囲を指定する方法
+            .Cells("A3:B4").Value = .Cells("A1:B2").Value
+            .Cells("A3:B4").BackColor = .Cells("A1:B2").BackColor
+            .Cells("A3:B4").ForeColor = .Cells("A1:B2").ForeColor
+
+            ' インデックス番号でセル範囲を指定する方法
+            .CopyRange(2, 2, 4, 2, 2, 2, False)
+        End With
+    End Sub
+
+    ' セル範囲のクリア：VBA互換APIを使う方法
+    Private Sub ClearCellA()
+        With FpSpread1.Sheets("Sheet1").AsWorksheet()
+            ' 文字列でセル範囲を指定する方法
+            .Range("A1:B4").Clear()
+
+            ' インデックス番号でセル範囲を指定する方法
+            .Range({New GrapeCity.Spreadsheet.Reference(2, 2, 5, 3)}).Clear()
+        End With
+    End Sub
+
+    ' セル範囲のクリア：従来の方法
+    Private Sub ClearCellB()
+        With FpSpread1.Sheets("Sheet1")
+            ' 文字列でセル範囲を指定する方法
+            .Cells("A1:B4").ResetValue()
+            .Cells("A1:B4").ResetBackColor()
+            .Cells("A1:B4").ResetForeColor()
+
+            ' インデックス番号でセル範囲を指定する方法
+            .ClearRange(2, 2, 4, 2, False)
+        End With
+    End Sub
+
+    ' セル範囲の選択：VBA互換APIを使う方法
+    Private Sub SelectCellA()
+        With FpSpread1.Sheets("Sheet1").AsWorksheet()
+            '' 文字列でセル範囲を指定する方法
+            '.Range("C3:D4").Select()
+
+            ' インデックス番号でセル範囲を指定する方法
+            .Range({New GrapeCity.Spreadsheet.Reference(2, 2, 3, 3)}).Select()
+        End With
+    End Sub
+
+    ' セル範囲の選択：従来の方法
+    Private Sub SelectCellB()
+        With FpSpread1.Sheets("Sheet1")
+            ' 文字列でセル範囲を指定する方法
+            ' 該当する機能はありません
+
+            ' インデックス番号でセル範囲を指定する方法
+            '.SetActiveCell(2, 2) ' <== 明示的なアクティブ枠の設定
+            .AddSelection(2, 2, 2, 2)
+        End With
+    End Sub
+
+    ' シートのコピー：VBA互換APIを使う方法
+    Private Sub CopySheetA()
+        With FpSpread1.Sheets("Sheet1").AsWorksheet()
+            ' ユニークなシート名が自動的に設定されます
+            .Copy(position:=0)
+        End With
+    End Sub
+
+    ' シートのコピー：従来の方法
+    Private Sub CopySheetB()
+        With FpSpread1.Sheets("Sheet1")
+            ' ユニークなシート名を明示的に設定しないと例外が発生します
+            Dim sheet = DirectCast(.Clone(), FarPoint.Win.Spread.SheetView)
+            sheet.SheetName = "Sheet2"
+            FpSpread1.Sheets.Insert(0, sheet)
+            'FpSpread1.ActiveSheetIndex = 0 ' アクティブシートの指定
+        End With
+    End Sub
+
+End Class
